8d3bb0407a41d25e72083eaebd6f65eb0262ec59
Anthony Simond <pv22nbo@eduvaud.ch>
2025-09-16 13:52:59 +0200

    exercice db done

--- routes/activities.js
+++ routes/activities.js
@@ -1,45 +1,40 @@
 import express from "express";
-import activities  from "../db/mock-activities.js";
+import {db} from  "../db/db-activities.js";
 
+//Code made in ase of success, we do not treat here the error case (id not existing for example)
 
 const activitiesRouter = express.Router();
-activitiesRouter.get("/",(req,res) =>{
+activitiesRouter.get("/",async(req,res) =>{
+    const activities = await db.getAllActivities();
     res.json({activities});
+
 });
 
-activitiesRouter.get("/:id",(req,res) => {
+activitiesRouter.get("/:id",async(req,res) => {
     const id = parseInt(req.params.id);
-    const activity = activities.find(activity => activity.id === id);
+    const activity = await db.getActivityById(id);
     res.json({activity})
 });
 
-activitiesRouter.delete("/:id",(req,res)=>{
+activitiesRouter.delete("/:id",async(req,res)=>{
     const id = parseInt(req.params.id);
-    const index = activities.findIndex(activity => activity.id === id);
-    activities.splice(index,1);
+    let deletedActivity = await db.deleteActivity(id)
     res.json({message: "Contact deleted"})
 });
 
-activitiesRouter.post("/",(req,res)=>{
+activitiesRouter.post("/",async(req,res)=>{
     const {name,start_date,time} = req.body;
-    //recupère le max des id existants
-    const maxId = activities.length > 0
-        ? Math.max(...activities.map(a => a.id))//fonction qui permet de prendre le plus grand id
-        :0; //si activities est vide le premier id sera 1.
-    const id = maxId + 1;
-    const newActivity={id,name,start_date,time};
-    activities.push(newActivity);
+    const newActivity= await db.createActivity({name,start_date,time});
     const message = `l'activité ${newActivity.name} a bien été crée !`;
     res.json({message : message, activity : newActivity});
 
 })
 
-activitiesRouter.put("/:id",(req,res)=>{
+activitiesRouter.put("/:id",async(req,res)=>{
     const id = parseInt(req.params.id);
     const {name,start_date,time} = req.body;
-    const index = activities.findIndex(activity => activity.id === id);
-    activities[index] = {id,name,start_date,time};
-    res.json({message : "Contact updated", activity : activities[index] });
+    const activityUpdated = await db.updateActivity(id,{name,start_date,time});
+    res.json({message : "Contact updated", activity : activityUpdated });
 
 })
 
